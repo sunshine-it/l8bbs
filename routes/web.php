e38712ab856dfa4d5a82437ac4c6d83a6834a556
Allen <pazmnew@163.com>
2021-10-20 10:07:54 +0000

    用户个人中心页面

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('users', 'UsersController', ['only'=>['show', 'update', 'edit']]);
