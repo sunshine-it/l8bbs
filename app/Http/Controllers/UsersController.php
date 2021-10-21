3ab7dbcfaab7fb1dae0ffa0dd88d57da17e1a955
Allen <pazmnew@163.com>
2021-10-21 11:32:49 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,12 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
-    //
+    // 个人中心
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // 编辑资料
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // 资料跟新
+    public function update(UserRequest $request, User $user)
+    {
+        // dd($request->all());
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '人资料更新成功！');
     }
 }
