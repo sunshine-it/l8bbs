81531635d1fb1d05f8e9ef1b736288de2e4ccb45
Allen <pazmnew@163.com>
2021-10-23 04:02:23 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,5 @@ Auth::routes(['verify' => true]);
 // Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('users', 'UsersController', ['only'=>['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
