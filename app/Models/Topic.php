31d864635b4d0eb5489e64c2e4f83a6a8541b0ce
Allen <pazmnew@163.com>
2021-10-29 06:20:26 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -59,4 +59,11 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    // 消息回复操作
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -12,9 +12,9 @@ class ReplyObserver
     public function created(Reply $reply)
     {
         // $reply->topic->increment('reply_count', 1);
-        $reply->topic->reply_count = $reply->topic->replies->count();
-        $reply->topic->save();
-
+        // $reply->topic->reply_count = $reply->topic->replies->count();
+        // $reply->topic->save();
+        $reply->topic->updateReplyCount();
         // 通知话题作者有新的评论
         $reply->topic->user->notify(new TopicReplied($reply));
     }
@@ -22,6 +22,14 @@ class ReplyObserver
     public function creating(Reply $reply)
     {
         $reply->content = clean($reply->content, 'user_topic_body');
+    }
+
+    // 消息回复已删除
+    public function deleted(Reply $reply)
+    {
+        // $reply->topic->reply_count = $reply->topic->replies->count();
+        // $reply->topic->save();
+        $reply->topic->updateReplyCount();
     }
 
 }
