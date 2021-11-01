d218c0d1194c2247849750fa24413e2b79813bcd
Allen <pazmnew@163.com>
2021-11-01 10:01:07 +0000

    首页话题列表

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,8 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 
 Auth::routes(['verify' => true]);
 
